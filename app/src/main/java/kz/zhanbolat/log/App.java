a3b77c972b4b2098f91663062ea86e052fdcf5f0
Zhanbolat Orakbayev <fous363@gmail.com>
2022-03-08 19:38:13 +0600

    Add Logback

--- app/src/main/java/kz/zhanbolat/log/App.java
+++ app/src/main/java/kz/zhanbolat/log/App.java
@@ -3,12 +3,14 @@
  */
 package kz.zhanbolat.log;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
+
+    private static final Logger logger = LoggerFactory.getLogger(App.class);
 
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        logger.info("Hello World");
     }
 }
