60a62e2c97934030ff832449e32abaadae562078
Zhanbolat Orakbayev <fous363@gmail.com>
2022-03-19 14:49:22 +0600

    Add SLF4J

--- app/src/main/java/kz/zhanbolat/log/App.java
+++ app/src/main/java/kz/zhanbolat/log/App.java
@@ -3,12 +3,17 @@
  */
 package kz.zhanbolat.log;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class App {
+    private static final Logger logger = LoggerFactory.getLogger(App.class);
+
     public String getGreeting() {
         return "Hello World!";
     }
 
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        logger.error("Error log");
     }
 }
